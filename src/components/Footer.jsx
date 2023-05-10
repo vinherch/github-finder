1ef35f0bd763777aec18cb369a4169944fe09bff
Vincenz Herzog <herzogv88@gmail.com>
2023-05-10 16:31:24 +0200

    Add Navbar & Footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
+      <div>
+        <p>Copyright &copy; {year} All rights reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+import PropTypes from "prop-types";
+
+const Navbar = ({ title }) => {
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="px-2 mx-2">
+          <Link to="/" className="text-lg font-bold">
+            <FaGithub className="inline pr-2 text-3xl" />
+            <p className="inline align-middle">{title}</p>
+          </Link>
+        </div>
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link className="btn btn-ghost btn-sm rounded-btm">Home</Link>
+            <Link className="btn btn-ghost btn-sm rounded-btm">About</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+
+export default Navbar;
