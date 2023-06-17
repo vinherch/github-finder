93eabd9e0b858fedaab90278dace78e15f2ef495
Vincenz Herzog <herzogv88@gmail.com>
2023-06-17 15:49:17 +0200

    Set actions to github context. Update github reducer

--- src/context/github/GitHubContext.js
+++ src/context/github/GitHubContext.js
@@ -4,25 +4,85 @@ import gitHubReducer from "./GitHubReducer";
 
 const GitHubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-
 export const GitHubProvider = ({ children }) => {
+  //Initial state - Reducer
   const initialState = {
     users: [],
+    user: {},
+    repositories: [],
     isLoading: false,
   };
 
   const [state, dispatch] = useReducer(gitHubReducer, initialState);
 
-  const fetchUsers = async () => {
-    const res = await fetch(`${GITHUB_URL}/users`);
+  //Search users from search component
+  const searchUsers = async (searchText) => {
+    setIsLoading();
+    /* Create search params */
+    const params = new URLSearchParams({
+      q: searchText,
+    });
+    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/search/users?${params}`);
     const data = res.ok ? await res.json() : null;
     if (data) {
-      dispatch({ type: "GET_USERS", payload: data });
+      dispatch({ type: "GET_USERS", data: data.items });
     }
   };
 
-  return <GitHubContext.Provider value={{ users: state.users, isLoading: state.isLoading, fetchUsers }}>{children}</GitHubContext.Provider>;
+  //Fetch single user / user details
+  const getUser = async (username) => {
+    setIsLoading();
+    //Fetch user data
+    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users/${username}`);
+    if (res.status === 404) {
+      window.location("/notfound");
+      return;
+    }
+    const data = res.ok ? await res.json() : null;
+    if (data) {
+      dispatch({ type: "GET_USER", data });
+    }
+  };
+
+  //Get repositories from user
+  const getUserRepositories = async (user) => {
+    setIsLoading();
+    const params = new URLSearchParams({
+      sort: "created",
+    });
+    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users/${user}/repos?${params}`);
+    const data = res.ok ? await res.json() : null;
+    if (data) {
+      dispatch({ type: "GET_REPOSITORIES", data });
+    }
+  };
+
+  /* Change isLoading state in reducer */
+  const setIsLoading = () => {
+    dispatch({ type: "SET_ISLOADING" });
+  };
+
+  /* Clear users from state */
+  const clearUsers = () => {
+    dispatch({ type: "CLEAR_USERS" });
+  };
+
+  return (
+    <GitHubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        isLoading: state.isLoading,
+        repositories: state.repositories,
+        searchUsers,
+        getUser,
+        clearUsers,
+        getUserRepositories,
+      }}
+    >
+      {children}
+    </GitHubContext.Provider>
+  );
 };
 
 export default GitHubContext;
--- src/context/github/GitHubReducer.js
+++ src/context/github/GitHubReducer.js
@@ -1,7 +1,15 @@
 const gitHubReducer = (state, action) => {
   switch (action.type) {
     case "GET_USERS":
-      return { ...state, users: action.payload, isLoading: false };
+      return { ...state, users: action.data, isLoading: false };
+    case "GET_USER":
+      return { ...state, user: action.data, isLoading: false };
+    case "GET_REPOSITORIES":
+      return { ...state, repositories: action.data, isLoading: false };
+    case "SET_ISLOADING":
+      return { ...state, isLoading: true };
+    case "CLEAR_USERS":
+      return { ...state, users: [] };
     default:
       return state;
   }
