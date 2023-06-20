f8f06dbc3a6e15325ab4f58755e9d4d68efd68dd
Vincenz Herzog <herzogv88@gmail.com>
2023-06-20 12:40:55 +0200

    Adjustments in RepositoryList styles

--- src/components/repositories/RepositoryList.jsx
+++ src/components/repositories/RepositoryList.jsx
@@ -6,7 +6,7 @@ import RepositoryItem from "./RepositoryItem";
 const RepositoryList = ({ repositories }) => {
   return (
     <div className=" rounded-lg shadow-lg card bg-base-100">
-      <div className="card-body">
+      <div className="card-body px-6">
         <h2 className="text-3xl my-4 font-bold card-title">Repositories</h2>
         {repositories.map((repository) => (
           <RepositoryItem key={repository.id} repository={repository} />
